1b747ead3527573c59cbf06fd1719e058a156127
Fourat Jebali <jebalifourat@issatso.u-sousse.tn>
2026-04-13 20:37:43 +0100

    Add auth frontend entry page

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    title: 'Authentication',
+    loadComponent: () =>
+      import('./features/auth/pages/auth-page/auth-page').then(
+        (module) => module.AuthPageComponent
+      ),
+  },
+  {
+    path: 'auth',
+    pathMatch: 'full',
+    redirectTo: '',
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
